c926f76a19600e292fed44ab740c25079300c0d8
00be <66236573+00be@users.noreply.github.com>
2023-03-12 21:59:31 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,4 @@ var nombre = prompt("Introduce tu nombre:")
 
 let myApp = document.getElementById("dom");
 
-alert (myApp.innerHTML);
-
 myApp.innerHTML = "Bienvenido a mi biografia " + nombre +"</br></br>" ;
